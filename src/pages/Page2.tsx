51fa1fab228fff884f133b134b6f9c818700292d
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-09-27 15:11:56 -0500

    Page2: changing swiper with carousel

--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -1,17 +1,39 @@
 import { ThemeContext } from "@/context/ThemeProvider";
+import { SpaceAnimations } from "@/layouts/SlideAnimations";
 import { motion } from 'framer-motion';
 import { useContext, useState } from "react";
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import { Autoplay, EffectCoverflow } from "swiper/modules";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import slide1 from "/page1/slides/space.png";
-import slide2 from "/page1/slides/krypto.png";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import slide3 from "/page1/slides/digency.png";
-import { SpaceAnimations } from "@/layouts/SlideAnimations";
+import slide2 from "/page1/slides/krypto.png";
+import slide1 from "/page1/slides/space.png";
 
 export default function Page2(props: { className: string }) {
     const { themeMode } = useContext(ThemeContext)
+
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 1,
+            // partialVisibilityGutter: 100
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1,
+            // partialVisibilityGutter: 30
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1,
+            // partialVisibilityGutter: 30
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            // partialVisibilityGutter: 0
+        },
+    };
 
     const [isHover, setHover] = useState(false)
     const variants = {
@@ -54,72 +76,59 @@ export default function Page2(props: { className: string }) {
                     }}
                 />}
                 <div className='relative z-[1] w-[99.5%] h-[99%] rounded-[20px] flex flex-col justify-center items-center bg-white dark:bg-[#242424]'>
-                    <Swiper
-                        modules={[Autoplay, EffectCoverflow]}
-                        effect={'coverflow'}
-                        rewind
-                        className='absolute z-0 w-full h-[80%] flex justify-center items-center'
-                        grabCursor={true}
-                        centeredSlides={true}
-                        spaceBetween={10}
-                        slidesPerView={'auto'}
-                        coverflowEffect={{
-                            rotate: 30,
-                            stretch: 0,
-                            depth: 100,
-                            modifier: 1,
-                            slideShadows: true,
-                        }}
-                        // loop={true}
-                        // autoplay={!isHover && {
-                        //     delay: 5000,
-                        //     disableOnInteraction: false,
-                        //     pauseOnMouseEnter: true
-                        // }}
-                        onSlideChange={changeSlide}
+                    <Carousel
+                        infinite
+                        // rewind
+                        // rewindWithAnimation
+                        autoPlay
+                        // autoPlay={autoPlay1}
+                        centerMode
+                        responsive={responsive}
+                        className="w-full h-[70%] owl-carousel owl-theme text-center"
+                        itemClass="px-1 flex justify-center"
+                        // autoPlaySpeed={100}
+                        // customTransition="transform 10s linear"
+                        // transitionDuration={10000}
+                        keyBoardControl={true}
+                        afterChange={changeSlide}
                     >
                         {images.map((img, i) => (
-                            <SwiperSlide
+                            <div
                                 key={`slide-${i}`}
-                                virtualIndex={i}
-                                className='w-[90%] flex justify-center items-center'
+                                className='relative w-full h-full flex justify-center items-center bg-transparent shadow-[10px_40px_10px_5px_black]'
+                                onClick={() => setHover(true)}
                             >
-                                <div
-                                    className='relative w-[80%] h-[80%] bg-transparent flex justify-center items-center shadow-[10px_40px_10px_5px_black]'
-                                    onClick={() => setHover(true)}
+                                <motion.img
+                                    key={`slide-img-${bgIndex}`}
+                                    className='object-contain rounded-lg'
+                                    src={img.src}
+                                    alt='library'
+                                    animate={{
+                                        scale: [0.9, 1],
+                                        // opacity: [0.5, 1]
+                                    }}
+                                    transition={{ duration: 0.5 }}
+                                />
+                                {bgIndex === i && <div className={`absolute ${isHover ? "hidden" : "flex"} bg-transparent w-[200px] h-[200px] -left-[100px] -bottom-[50px]`}>
+                                    {img.animations}
+                                </div>}
+                                {bgIndex === i && <motion.div
+                                    className={`absolute ${isHover ? "bg-opacity-80" : "opacity-0 z-[-1]"} z-[10] cursor-pointer rounded-xl w-full h-full bg-black p-2`}
+                                    animate={isHover ? "open" : "close"}
+                                    variants={variants}
+                                    transition={{ duration: 0.5 }}
+                                    onMouseLeave={() => setHover(false)}
                                 >
-                                    <motion.img
-                                        key={`slide-img-${bgIndex}`}
-                                        className='object-contain rounded-lg'
-                                        src={img.src}
-                                        alt='library'
-                                        animate={{
-                                            scale: [0.9, 1],
-                                            // opacity: [0.5, 1]
-                                        }}
-                                        transition={{ duration: 0.5 }}
-                                    />
-                                    <div className='absolute bg-white w-[200px] h-[200px] -left-[100px] -bottom-[50px]'>
-                                        {img.animations}
-                                    </div>
-                                </div>
-                            </SwiperSlide>
+                                    <h3>Project {bgIndex}</h3>
+                                    <p>Description</p>
+                                    <ul>
+                                        <li>Tech Stack</li>
+                                    </ul>
+                                </motion.div>}
+                            </div>
                         )
                         )}
-                    </Swiper>
-                    <motion.div
-                        className={`absolute ${isHover ? "bg-opacity-80" : "opacity-0 z-[-1]"} z-[1] rounded-xl w-[70%] h-[70%] bg-black p-2`}
-                        animate={isHover ? "open" : "close"}
-                        variants={variants}
-                        transition={{ duration: 0.5 }}
-                        onClick={() => setHover(false)}
-                    >
-                        <h3>Project {bgIndex}</h3>
-                        <p>Description</p>
-                        <ul>
-                            <li>Tech Stack</li>
-                        </ul>
-                    </motion.div>
+                    </Carousel>
                 </div>
             </div>
         </div>
