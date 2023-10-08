17eddec043d0b518707edfcc83ef1afdc056180c
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-10-08 10:07:58 -0500

    Page2: better animations

--- src/layouts/SlideAnimations.tsx
+++ src/layouts/SlideAnimations.tsx
@@ -9,9 +9,9 @@ import slide3Megaphone from "/page2/slides/digency/icons8-megaphone-94.png";
 import slide3Movie from "/page2/slides/digency/icons8-movie-94.png";
 import slide3Wedding from "/page2/slides/digency/icons8-wedding-day-94.png";
 import slide3Youtube from "/page2/slides/digency/icons8-youtube-94.png";
-import slide2Bitcoin from "/page2/slides/krypto/bitcoin.png";
-import slide2Building from "/page2/slides/krypto/building.png";
-import slide2Etherium from "/page2/slides/krypto/etherium.png";
+import slide2Wallet from "/page2/slides/krypto/icons8-digital-wallet-64.png";
+import slide2Ethereum from "/page2/slides/krypto/icons8-ethereum-96.png";
+import slide2Nft from "/page2/slides/krypto/icons8-nft-60.png";
 import slide1Clouds from "/page2/slides/space/clouds.png";
 import slide1Planet from "/page2/slides/space/planet.png";
 import slide1Rocket from "/page2/slides/space/rocket.png";
@@ -19,11 +19,11 @@ import slide1Rocket from "/page2/slides/space/rocket.png";
 export const SpaceAnimations = () => {
     return <>
         <motion.img
-            className='absolute w-[70px] h-[70px] bottom-3 right-1/3'
+            className='absolute w-[40px] h-[40px] left-8'
             src={slide1Planet}
             alt={slide1Planet}
             animate={{
-                scale: [1, 0.9, 1]
+                translateY: [0, -10, 0]
             }}
             transition={{
                 duration: 3,
@@ -32,7 +32,7 @@ export const SpaceAnimations = () => {
             }}
         />
         <motion.img
-            className='absolute w-[200px] h-[50px] bottom-0 left-0'
+            className='absolute w-[180px] h-[40px] bottom-3'
             src={slide1Clouds}
             alt={slide1Clouds}
             animate={{
@@ -45,7 +45,7 @@ export const SpaceAnimations = () => {
             }}
         />
         <motion.img
-            className='absolute w-[90px] h-[160px] bottom-0 right-3'
+            className='absolute w-[70px] h-[120px]'
             src={slide1Rocket}
             alt={slide1Rocket}
             animate={{
@@ -63,9 +63,9 @@ export const SpaceAnimations = () => {
 export const KryptoAnimations = () => {
     return <>
         <motion.img
-            className='absolute w-[100px] h-[160px] bottom-3 right-1/4'
-            src={slide2Building}
-            alt={slide2Building}
+            className='absolute w-[100px] h-[100px] bottom-3'
+            src={slide2Wallet}
+            alt={slide2Wallet}
             animate={{
                 scale: [1, 0.9, 1]
             }}
@@ -74,24 +74,27 @@ export const KryptoAnimations = () => {
                 repeat: Infinity,
                 easeInOut: 'linear'
             }}
+            onAuxClick={() => window.open('https://icons8.com/icon/H8NM1TB8Ho5e/digital-wallet', '_blank', 'noopener noreferrer')}
         />
         <motion.img
-            className='absolute w-[60px] h-[70px] bottom-0 left-0'
-            src={slide2Etherium}
-            alt={slide2Etherium}
+            className='absolute w-[50px] h-[50px] top-5'
+            src={slide2Ethereum}
+            alt={slide2Ethereum}
             animate={{
-                translateY: [10, 0, 10]
+                translateY: [10, 0, 10],
+                rotateY: [180, 0, 180]
             }}
             transition={{
                 duration: 3,
                 repeat: Infinity,
                 easeInOut: 'linear'
             }}
+            onAuxClick={() => window.open('https://icons8.com/icon/IhWBOFHtv6vx/ethereum', '_blank', 'noopener noreferrer')}
         />
         <motion.img
-            className='absolute w-[60px] h-[70px] bottom-0 right-0'
-            src={slide2Bitcoin}
-            alt={slide2Bitcoin}
+            className='absolute w-[60px] h-[60px] bottom-0 left-0'
+            src={slide2Nft}
+            alt={slide2Nft}
             animate={{
                 translateY: [0, 10, 0]
             }}
@@ -100,6 +103,7 @@ export const KryptoAnimations = () => {
                 repeat: Infinity,
                 easeInOut: 'linear'
             }}
+            onAuxClick={() => window.open('https://icons8.com/icon/3KPaRW3427k0/nft', '_blank', 'noopener noreferrer')}
         />
     </>
 }
@@ -107,7 +111,7 @@ export const KryptoAnimations = () => {
 export const DigencyAnimations = () => {
     return <>
         <motion.img
-            className='absolute w-[70px] h-[60px] top-3 left-3'
+            className='absolute w-[60px] h-[50px] top-3 left-3'
             src={slide3Youtube}
             alt={slide3Youtube}
             animate={{
@@ -121,7 +125,7 @@ export const DigencyAnimations = () => {
             onAuxClick={() => window.open('https://icons8.com/icon/qLVB1tIe9Ts9/youtube', '_blank', 'noopener noreferrer')}
         />
         <motion.img
-            className='absolute w-[70px] h-[70px] bottom-3 left-3'
+            className='absolute w-[60px] h-[60px] bottom-3 left-3'
             src={slide3Wedding}
             alt={slide3Wedding}
             animate={{
@@ -135,7 +139,7 @@ export const DigencyAnimations = () => {
             onAuxClick={() => window.open('https://icons8.com/icon/HFfgVaJNpYdO/wedding-day', '_blank', 'noopener noreferrer')}
         />
         <motion.img
-            className='absolute w-[70px] h-[70px] bottom-3 right-3'
+            className='absolute w-[60px] h-[60px] bottom-3 right-3'
             src={slide3Movie}
             alt={slide3Movie}
             animate={{
@@ -149,7 +153,7 @@ export const DigencyAnimations = () => {
             onAuxClick={() => window.open('https://icons8.com/icon/F2R6V3ECtTmF/documentary', '_blank', 'noopener noreferrer')}
         />
         <motion.img
-            className='absolute w-[70px] h-[70px] top-3 right-3'
+            className='absolute w-[60px] h-[60px] top-3 right-3'
             src={slide3Megaphone}
             alt={slide3Megaphone}
             animate={{
@@ -196,7 +200,7 @@ export const BocheAnimations = () => {
             onAuxClick={() => window.open('https://icons8.com/icon/2QbGlbjEvTes/teddy-bear', '_blank', 'noopener noreferrer')}
         />
         <motion.img
-            className='absolute w-[80px] h-[80px] bottom-4 left-3'
+            className='absolute w-[70px] h-[70px] bottom-4 left-3'
             src={slide4Brick}
             alt={slide4Brick}
             animate={{
@@ -210,7 +214,7 @@ export const BocheAnimations = () => {
             onAuxClick={() => window.open('https://icons8.com/icon/yWDrzc0ldyqH/brick', '_blank', 'noopener noreferrer')}
         />
         <motion.img
-            className='absolute w-[80px] h-[80px] bottom-4 right-3'
+            className='absolute w-[70px] h-[70px] bottom-4 right-3'
             src={slide4Horse}
             alt={slide4Horse}
             animate={{
