114a4899451c5b10eb0d5b0faf17aeb9e3621a13
Tiber <danilo.ochoa.hidalgo@outlook.com>
2024-09-14 16:38:58 -0500

    fix: update resumes 2024

--- src/context/LangProvider.tsx
+++ src/context/LangProvider.tsx
@@ -4,26 +4,28 @@ import { createContext, useState } from "react"
 
 type LangType = 'es' | 'en'
 
+const DEFAULT_LANG = 'en'
+const LANGUAGES = {
+    es: textES,
+    en: textEN,
+}
+
 export const LangContext = createContext({
-    lang: "es",
-    text: textES,
+    lang: DEFAULT_LANG,
+    text: LANGUAGES[DEFAULT_LANG],
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     changeLang: (_lang: LangType) => { }
 })
 
 export default function LangProvider(props: { children: JSX.Element }) {
-    const langs = {
-        es: textES,
-        en: textEN
-    }
 
-    const [lang, setLang] = useState<LangType>(localStorage.getItem('lang') as LangType || 'es')
-    const [text, setText] = useState(langs[localStorage.getItem('lang') as LangType || 'es'])
+    const [lang, setLang] = useState<LangType>(localStorage.getItem('lang') as LangType || DEFAULT_LANG)
+    const [text, setText] = useState(LANGUAGES[localStorage.getItem('lang') as LangType || DEFAULT_LANG])
 
     const changeLang = (lang: LangType) => {
         localStorage.setItem('lang', lang)
         setLang(lang)
-        setText(langs[lang])
+        setText(LANGUAGES[lang])
     }
 
     return <LangContext.Provider value={{ lang, text, changeLang }}>
